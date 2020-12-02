7f689b3ddf8bc176f1ccadb67c804b2b45e36a4c
Spencer Firlik <safirlik@mtu.edu>
2020-12-02 14:16:07 -0500

    WolframURI Encoding Fix

--- app/javascript/packs/problemSelection.js
+++ app/javascript/packs/problemSelection.js
@@ -432,6 +432,8 @@ function wolfram(){
     console.log("BEFORE URI " + e);
     const wolframURL = "https://api.wolframalpha.com/v2/query?appid=GRWHG2-8TQ9WK8J4J&input=logic circuit " + e + "&output=json";
     let wolframURI = encodeURI(wolframURL);
+    wolframURI = wolframURI.replace(/\(/g, '%28');
+    wolframURI = wolframURI.replace(/\)/g, '%29');
     console.log("AFTER URI " + wolframURI);
     const xhr = new XMLHttpRequest();
 
