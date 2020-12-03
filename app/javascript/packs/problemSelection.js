b5b0524010a4080ffd610a1313bd9a0803e526e8
Spencer Firlik <safirlik@mtu.edu>
2020-12-03 16:14:53 -0500

    Expression Algorithm Modification

--- app/javascript/packs/problemSelection.js
+++ app/javascript/packs/problemSelection.js
@@ -259,6 +259,7 @@ function buildExpression(expression) {
         console.log(x);
         let r = Math.floor(Math.random()*5);
         match = String.fromCharCode(65+r);
+        console.log("match");
         e += match; 
         console.log("Expression " + e);
         console.log("Number of times "+i);
