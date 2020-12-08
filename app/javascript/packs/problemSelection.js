427fd74b55fc74e6f81ae176e1ddb8ebb0d03a6e
Spencer Firlik <safirlik@mtu.edu>
2020-12-07 22:07:36 -0500

    Styling and Show Answer Function Addition

--- app/javascript/packs/problemSelection.js
+++ app/javascript/packs/problemSelection.js
@@ -602,8 +602,8 @@ function getResult(result){
 function getResultTruth(result){
     tableAnswer = [];
     tableAnswerFinal = "";
-    for(i=0;i<userInput.length;i++){
-        tableAnswer.push(userInput[i].value);    
+    for(i=0;i<truth.length;i++){
+        tableAnswer.push(truth[i].value);    
     }
     tableAnswerFinal = tableAnswer.toString();
     console.log("TableAnswer " + tableAnswerFinal);
